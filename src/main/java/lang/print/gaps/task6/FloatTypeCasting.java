10ada20c1aea02b69721e71145980c0dee4565a1
DavidMichaelson <dovydasbalciunas95@gmail.com>
2025-04-24 13:50:49 +0300

    done task although the condition of PrePostInrcement and OperatorPrecedence was wrong

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,14 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+
+        System.out.println(Math.round(numberToBeRounded));
+    }
+
+    public static void main(String[] args) {
+
+        FloatTypeCasting casting = new FloatTypeCasting();
+        casting.roundNumber(3.6f);
+        casting.roundNumber(-1.6f);
     }
 }
